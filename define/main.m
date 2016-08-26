a989d60e99872cf18a32ad03469c7d8c74e6d8dc
gamma <gamma@gammas-mac-mini.local>
2016-08-25 17:24:32 -0700

    Initial Commit

---
+++ define/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  define
+//
+//  Created by gamma on 8/25/16.
+//  Copyright © 2016 Beau Galbraith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
